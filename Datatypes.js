a15829942512da6017504eb037f74a6f5d59e496
Shresth Sahai <68563453+shresth-sahai@users.noreply.github.com>
2024-05-15 21:40:55 +0530

    Create Datatypes.js

---
+++ Datatypes.js
@@ -0,0 +1,53 @@
+// Data types in JS 
+// String, Number , BigInt, Boolean, Undefined,Null, Symbol,Object(object,array,Date)
+//console.log(this)
+// let w = 7.6;
+// let color = "Yellow";
+// let x = true;
+// const obj = { first: "ABC", second: "XYZ" };//object
+// // array object 
+// const nums = [1, 2, 3]
+//console.log(typeof nums);
+//a = new Date();
+//console.log(a);
+//console.log(typeof a);
+// let a = 16 + "V";
+// let b = "V" + 16
+// let c = 16+4 + "V"  // js evaluates exp from L -> R
+//console.log(c);
+// let x=1;
+// x = 5;
+// x = "ABC";
+// let d = "ABC";
+// let e = 'ABC'
+
+// console.log(d == e); // data
+// console.log(d === e);// dta +type
+//typeof (3 + 4)
+//let x;
+//console.log(x)
+
+
+
+// console.log(typeof a )
+// K_V pair
+// const obj = {
+//     A: "1",
+//     B: "2",
+//     concat: function c() {
+//         return this.A + this.B;
+//     }
+// }
+// console.log(obj.concat())
+// this ---> inside obj will point to current object 
+// this console.log(this)--> global object 
+
+
+
+
+
+
+
+
+
+
